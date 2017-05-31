0aeaac90233ff3b6c39c58384c3a00fc56e35118
twoflyliu <twoflyliu@gmail.com>
2017-05-31 22:38:29 +0800

    add some read me information

--- run.sh
+++ run.sh
@@ -4,6 +4,14 @@
 # Mail: twoflyliu@163.com
 # Create time: 2017 05 28 08:08:52
 
+# 这个脚本是开发人员使用的, 最终的应用的是novel/app.py
+# 需要安装python3, 和pygobject库
+# 可以直接使用/distrib目录的debian包，内部使用golang和gtk+开发的
+# golang开发后端， gtk+开发前缀，并且gnome桌面效果最好
+#
+# 理论上可以移植到windows上，但是显示效果不好，依赖包库超级多，
+# 后面会推出不适用gtk+，直接使用win32 + duilib来进行开发
+
 SCRIPT_DIR=${0%/*}
 PWD_DIR=$(pwd)
 
